78037c4d718d008b1ca98e95a7c0942d371e3b7a
YahyaMasri <yehiamasri83@gmail.com>
2023-03-01 20:47:34 +0200

    Route for  the evaluation

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EvaluationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+// Evaluation Route
+Route::Post('/evaluation', [EvaluationController::class,'addEvaluation']);
+Route::Get('/evaluation', [EvaluationController::class, 'getEvaluation']);
+Route::Get('/evaluation/{id}',[EvaluationController::class, 'getEvaluationById']);
+Route::Patch('/evaluation/{id}', [EvaluationController::class, 'updateEvaluation']);
+Route::Delete('/evaluation/{id}', [EvaluationController::class, 'deleteEvaluation']);
