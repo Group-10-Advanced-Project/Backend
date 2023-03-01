d6643b71d3adf74beb7c02db39505db2d946a530
zahraa mazloum <zahraamazloum2001@gmail.com>
2023-03-01 20:24:46 +0200

    kpi controller

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KpiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::Post('/kpi',[KpiController::class,'addKpi']);
+Route::Get('/getkpi/{id}',[KpiController::class,'getKpi']);
+Route::delete('/deletekpi/{id}',[KpiController::class,'deleteKpi']);    
+Route::Put('/editKpi/{id}',[KpiController::class,'editKpi']);    
