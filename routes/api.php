11af3e2363e22a32320bb83cd4e1ca2f40762b01
Amen <jamen.jamne30@gmail.com>
2023-03-02 09:00:58 +0200

    auth finished

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -25,6 +26,16 @@ Route::Get('/admin', [AdminController::class, "getAllAdmins"]);
 Route::Get('/admin/{id}', [AdminController::class, "getAdminByID"]);
 Route::Delete('/admin/{id}', [AdminController::class, "deleteAdmin"]);
 Route::Patch('/admin/{id}', [AdminController::class, "editAdmin"]);
+
+// login and logout
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::Post('/addAdmin', [AdminController::class, "addAdmin"]);
+    Route::Post('/login', [AdminController::class, "login"]);
+    Route::Post('/logout', [AdminController::class, "logout"]);
+});
 
 //employee routes
 Route::Post('/employee',[EmployeeController::class,'addEmployee']);
